e8ca4c8e42ce2521903ba9e43efd0d69c7a62717
John Doe <johndoe@example.com>
2024-01-03 18:41:57 +0100

    Updated sql options

--- backend/connect.js
+++ backend/connect.js
@@ -2,7 +2,7 @@ import mysql2 from 'mysql2'
 
 export const db = mysql2.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: 'admin',
+  user: 'user',
+  password: 'password',
   database: 'cars',
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,47 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect } from 'react'
 import {
   createBrowserRouter,
   RouterProvider,
   Outlet,
   Navigate,
-} from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Register from "./pages/register/Register";
-import Login from "./pages/login/Login";
-import AddAuction from "./pages/addAuction/AddAuction";
-import Auction from "./pages/auction/Auction";
-import Home from "./components/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import Profile from "./pages/profile/Profile";
-import "./style.scss";
-import { NavbarProvider } from "./context";
+} from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Register from './pages/register/Register'
+import Login from './pages/login/Login'
+import AddAuction from './pages/addAuction/AddAuction'
+import Auction from './pages/auction/Auction'
+import Home from './components/home/Home'
+import Navbar from './components/navbar/Navbar'
+import Profile from './pages/profile/Profile'
+import './style.scss'
+import { NavbarProvider } from './context'
 
 const App = () => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient()
 
   const Layout = () => {
     return (
       <QueryClientProvider client={queryClient}>
         <div>
           <Navbar />
-          <div style={{ display: "flex" }}>
+          <div style={{ display: 'flex' }}>
             <Outlet />
           </div>
         </div>
       </QueryClientProvider>
-    );
-  };
+    )
+  }
 
   const ProtectedRoute = ({ children }) => {
-    if (!localStorage.getItem("user")) {
-      return <Navigate to="/login" />;
+    if (!localStorage.getItem('user')) {
+      return <Navigate to="/login" />
     }
-    return children;
-  };
+    return children
+  }
 
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: (
         <ProtectedRoute>
           <Layout />
@@ -49,45 +49,45 @@ const App = () => {
       ),
       children: [
         {
-          path: "/",
+          path: '/',
           element: <Home />,
         },
         {
-          path: "/addauction",
+          path: '/addauction',
           element: <AddAuction />,
         },
 
         {
-          path: "/auction/:id",
+          path: '/auction/:id',
           element: <Auction />,
         },
         {
-          path: "/myauctions",
+          path: '/myauctions',
           element: <Home />,
         },
         {
-          path: "/favourite",
+          path: '/favourite',
           element: <Home />,
         },
         {
-          path: "/profile",
+          path: '/profile',
           element: <Profile />,
         },
       ],
     },
     {
-      path: "/login",
+      path: '/login',
       element: <Login />,
     },
     {
-      path: "/register",
+      path: '/register',
       element: (
         <QueryClientProvider client={queryClient}>
           <Register />
         </QueryClientProvider>
       ),
     },
-  ]);
+  ])
 
   return (
     <div>
@@ -95,7 +95,7 @@ const App = () => {
         <RouterProvider router={router} />
       </NavbarProvider>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
